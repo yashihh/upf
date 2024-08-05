9b21106296fa2bfca21801e8b0b2f5ff231f3a21
yashihh <tseng890131@gmail.com>
2024-08-05 16:55:35 +0800

    test port status ok!

--- internal/nwtt/pmic.go
+++ internal/nwtt/pmic.go
@@ -3,10 +3,11 @@ package nwtt
 import (
 	"encoding/binary"
 	"sort"
+
+	"github.com/pkg/errors"
 )
 
 func (n *NWTTServer) EncodePortManagementCapability(portNumber uint32) ([]byte, error) {
-	/* TODO: Get capabilities from structure*/
 	portCapability := []byte{}
 	b := make([]byte, 2)
 	capabilities := []uint16{}
@@ -25,4 +26,61 @@ func (n *NWTTServer) EncodePortManagementCapability(portNumber uint32) ([]byte, 
 	}
 
 	return portCapability, nil
+}
+
+func (n *NWTTServer) EncodePortStatus(portNumber uint32, parameter uint16, value ...byte) ([]byte, error) {
+	/* TODO: Get status from AF-request */
+	n.log.Infof("Build PortStatus = [0x%x]", parameter)
+
+	portStatus := []byte{}
+	parameterName := make([]byte, 2)
+	length := make([]byte, 2)
+	buffer := value
+
+	binary.BigEndian.PutUint16(parameterName, parameter)
+
+	switch parameter {
+	case PMIC_SupportedPTPInstanceTypes:
+		if len(value) == 0 { // default
+			buffer = append(buffer, BoundaryClock)
+			buffer = append(buffer, E2ETransparentClock)
+		}
+	case PMIC_SupportedTransportTypes: // only support ipv4 currently
+		if len(value) == 0 { // default
+			buffer = append(buffer, IPv4)
+		}
+	case PMIC_SupportedDelayMechanisms: // only support E2E currently
+		if len(value) == 0 { // default
+			buffer = append(buffer, E2E)
+		}
+	case PMIC_PTPGrandmasterCapable:
+		if len(value) == 0 { // default
+			buffer = append(buffer, TRUE)
+		}
+	case PMIC_gPTPGrandmasterCapable:
+		if len(value) == 0 { // default
+			buffer = append(buffer, FALSE)
+		}
+	case PMIC_SupportedPTPProfiles:
+		if len(value) == 0 { // default
+			buffer = append(buffer, E2EDefault)
+		}
+	case PMIC_NumberOfSupportedPTPInstances:
+	case PMIC_PTPInstanceList:
+	default:
+		return nil, errors.Errorf("Reading unknown parameter:[%v]", parameter)
+	}
+
+	binary.BigEndian.PutUint16(length, uint16(len(buffer)))
+
+	/* Put Name of parameter */
+	portStatus = append(portStatus, parameterName...)
+
+	/* Put Length of Port parameter value */
+	portStatus = append(portStatus, length...)
+
+	/* Put Port parameter value */
+	portStatus = append(portStatus, buffer...)
+
+	return portStatus, nil
 }
