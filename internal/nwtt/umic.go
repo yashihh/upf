7a1e76dbc5faeff9c29542bd6e7b83f3b7acee48
yashihh <tseng890131@gmail.com>
2024-08-09 18:52:52 +0800

    add umic and test successfully TODO: pmic encode

--- internal/nwtt/umic.go
+++ internal/nwtt/umic.go
@@ -3,57 +3,10 @@ package nwtt
 import (
 	"encoding/binary"
 	"sort"
-	"time"
+
+	"github.com/pkg/errors"
+	"github.com/wmnsk/go-pfcp/ie"
 )
-
-type ClockIdentity [8]byte
-
-type PortIdentity struct {
-	ClockIdentity ClockIdentity
-	PortNumber    uint16
-}
-
-type PortState byte
-
-const (
-	INITIALIZING PortState = iota + 1
-	FAULTY
-	DISABLED
-	LISTENING
-	PRE_MASTER
-	MASTER
-	PASSIVE
-	UNCALIBRATED
-	SLAVE
-)
-
-type DelayMechanism byte
-
-const (
-	E2E DelayMechanism = iota + 1
-	P2P
-	COMMON_P2P
-	SPECIAL
-	NO_MECHANISM DelayMechanism = 0xFE
-)
-
-/*
-	PORT DATA SET (IEEE 1588)
-*/
-type PortDS struct {
-	PortIdentity            PortIdentity
-	PortState               PortState
-	LogMinDelayReqInterval  int8
-	PeerMeanPathDelay       time.Duration
-	LogAnnounceInterval     int8
-	AnnounceReceiptTimeout  uint8
-	LogSyncInterval         int8
-	DelayMechanism          DelayMechanism
-	LogMinPdelayReqInterval int8
-	VersionNumber           uint8
-	DelayAsymmetry          int64
-	PortEnable              bool // optional
-}
 
 func (n *NWTTServer) EncodeUserPlaneNodeManagementCapability() ([]byte, error) {
 	/* TODO: Get capabilities from structure*/
@@ -75,4 +28,146 @@ func (n *NWTTServer) EncodeUserPlaneNodeManagementCapability() ([]byte, error) {
 	}
 
 	return userPlaneNodeCapability, nil
+}
+func (n *NWTTServer) HandleManageUserPlaneNodeCommand(managementList []byte) ([]byte, error) {
+	buffer := []byte{}
+	StatusContents := []byte{}
+	var readNum uint8 = 0
+	iEI := uint8(managementList[0])
+	length := binary.BigEndian.Uint16(managementList[1:3])
+	if int(length) != len(managementList[3:]) {
+		return nil, errors.New("BMIC Length IE mismatch with length of Port management list contents")
+	}
+	n.log.Infof("HandleManageUserPlaneNodeCommand IEI: [%d], length:[%d], len(list):[%d]", iEI, length, len(managementList[3:]))
+	for idx := 3; idx < int(length)+3; {
+		switch uint8(managementList[idx]) {
+		case GetCapabilities:
+			n.log.Debugln("Handle Port GetCapabilities Operation")
+			contents, err := n.EncodeUserPlaneNodeManagementCapability()
+			if err != nil {
+				n.log.Errorln(err)
+				return nil, err
+			}
+			/* Insert length before contents */
+			oplength := make([]byte, 2)
+			binary.BigEndian.PutUint16(oplength, uint16(len(contents)))
+			contents = append(oplength, contents...)
+			/* Insert port management capability IEI */
+			contents = append([]byte{PortManagementCapabilityIEI}, contents...)
+			/* Append buffer */
+			buffer = append(buffer, contents...)
+			idx += 1
+		case ReadParameter:
+			n.log.Infof("Handle UserPlaneNode ReadParameter Operation")
+			parameter := binary.BigEndian.Uint16(managementList[idx+1 : idx+3])
+			contents, err := n.EncodeUserPlaneNodeStatus(parameter)
+			if err != nil {
+				n.log.Errorln(err)
+				return nil, err
+			}
+			readNum++
+			StatusContents = append(StatusContents, contents...)
+			idx += 3
+		case SetParameter:
+			n.log.Infof("Handle UserPlaneNode SetParameter Operation")
+			/* TODO：Hangle correct idx*/
+			idx += 3
+		case SubscribeNotifyForParameter:
+			n.log.Infof("Handle UserPlaneNode SubscribeNotifyForParameter Operation")
+			idx += 3
+		case UnsubscribeForParameter:
+			n.log.Infof("Handle UserPlaneNode UnsubscribeForParameter Operation")
+			idx += 3
+		default:
+			return nil, errors.Errorf("Unsupport operation code in Manage UserPlaneNode Command")
+		}
+
+	}
+	if readNum != 0 {
+		/* Put Number of port parameters successfully read into Status */
+		StatusContents = append([]byte{readNum}, StatusContents...)
+		/* Put Length of port status and error contents */
+		statusLength := make([]byte, 2)
+		binary.BigEndian.PutUint16(statusLength, uint16(len(StatusContents)))
+		StatusContents = append(statusLength, StatusContents...)
+		/* Put Port status IEI  */
+		StatusContents = append([]byte{PortStatusIEI}, StatusContents...)
+		/* Merge to buffer*/
+		buffer = append(buffer, StatusContents...)
+	}
+	return buffer, nil
+}
+
+func (n *NWTTServer) DecodeUserPlaneNodeManagementInformation(BMIC []byte) (*ie.IE, error) {
+	switch uint8(BMIC[0]) {
+	case ManageUserPlaneNodeCommand:
+		buffer, err := n.EncodeUserPlaneNodeManagementCapability()
+		n.log.Infof("buffer length = %v", len(buffer))
+		if err != nil {
+			n.log.Errorln(err)
+			return nil, err
+		}
+		/* insert Manage Bridge Complete id*/
+		buffer = append([]byte{ManageUserPlaneNodeComplete}, buffer...)
+		/* Return buffer wrap in BMIC */
+		return ie.NewBridgeManagementInformationContainer(string(buffer)), nil
+	case ManageUserPlaneNodeComplete:
+	case UserPlaneNodeManagementNotify:
+	case UserPlaneNodeManagementNotifyAck:
+	default:
+	}
+	return nil, nil
+}
+
+func (n *NWTTServer) EncodeUserPlaneNodeStatus(parameter uint16) ([]byte, error) {
+	/* TODO: move default value to config */
+	n.log.Infof("Build User Plane Node Status = [0x%x]", parameter)
+
+	portStatus := []byte{}
+	parameterName := make([]byte, 2)
+	length := make([]byte, 2)
+	buffer := []byte{}
+	binary.BigEndian.PutUint16(parameterName, parameter)
+
+	switch parameter {
+	case SupportedPTPInstanceTypes:
+		buffer = append(buffer, BoundaryClock)
+		buffer = append(buffer, E2ETransparentClock)
+
+	case SupportedTransportTypes: // only support ipv4 currently
+		buffer = append(buffer, IPv4)
+
+	case SupportedDelayMechanisms: // only support E2E currently
+		buffer = append(buffer, E2E)
+
+	case PTPGrandmasterCapable:
+		buffer = append(buffer, TRUE)
+
+	case gPTPGrandmasterCapable:
+		buffer = append(buffer, FALSE)
+
+	case SupportedPTPProfiles:
+		buffer = append(buffer, E2EDefault)
+
+	case NumberOfSupportedPTPInstances: // currently support only one instance
+		value := make([]byte, 2)
+		binary.BigEndian.PutUint16(value, 1)
+		buffer = append(buffer, value...)
+
+	default:
+		return nil, errors.Errorf("Reading unknown parameter:[%v]", parameter)
+	}
+
+	binary.BigEndian.PutUint16(length, uint16(len(buffer)))
+
+	/* Put Name of parameter */
+	portStatus = append(portStatus, parameterName...)
+
+	/* Put Length of Port parameter value */
+	portStatus = append(portStatus, length...)
+
+	/* Put Port parameter value */
+	portStatus = append(portStatus, buffer...)
+
+	return portStatus, nil
 }
---
+++ internal/nwtt/umic_test.go
@@ -0,0 +1,105 @@
+package nwtt
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/free5gc/go-upf/internal/forwarder"
+	"github.com/free5gc/go-upf/pkg/factory"
+)
+
+func TestUMIC(t *testing.T) {
+	cfg := &factory.Config{
+		NWTT: &factory.NWTT{
+			UpNodeID:  "02:00:5e:10:00:00:00:01",
+			NwttPorts: "2,3",
+			DsttPorts: "4",
+		},
+	}
+	n, err := NewNWTTServer(
+		cfg,
+		forwarder.Empty{},
+	)
+	n.Init()
+	t.Run("Create NWTT Server", func(t *testing.T) {
+
+		if n == nil {
+			t.Errorf("NewNWTTServer Create failed; got %v\n", err)
+		}
+	})
+	t.Run("Encode User Plane Node Management Capability", func(t *testing.T) {
+		truth := []byte{0, 0x01, 0, 0x03, 0, 0x04}
+		tmp, err := n.EncodeUserPlaneNodeManagementCapability()
+		if err != nil {
+			t.Errorf("Encode User Plane Node Management Capability failed; got %v\n", err)
+		}
+		if !bytes.Equal(truth, tmp) {
+			t.Errorf("Encode User Plane Node Management Capability wrong, want %v but got %v", truth, tmp)
+		}
+	})
+	t.Run("Encode UserPlaneNode Status with Supported PTP instance types", func(t *testing.T) {
+		truth := []byte{0, 0x74, 0, 2, BoundaryClock, E2ETransparentClock}
+		tmp, err := n.EncodeUserPlaneNodeStatus(SupportedPTPInstanceTypes)
+		if err != nil {
+			t.Errorf("Encode Supported PTP instance types failed; got %v\n", err)
+		}
+		if !bytes.Equal(truth, tmp) {
+			t.Errorf("Encode Supported PTP instance types wrong, want %v but got %v", truth, tmp)
+		}
+	})
+
+	t.Run("Encode EncodeUserPlaneNode Status with Supported transport types", func(t *testing.T) {
+		truth := []byte{0, 0x75, 0, 1, IPv4}
+		tmp, err := n.EncodeUserPlaneNodeStatus(SupportedTransportTypes)
+		if err != nil {
+			t.Errorf("Encode Supported transport types failed; got %v\n", err)
+		}
+		if !bytes.Equal(truth, tmp) {
+			t.Errorf("Encode Supported transport types, want %v but got %v", truth, tmp)
+		}
+	})
+
+	t.Run("Encode EncodeUserPlaneNode Status with Supported Delay Mechanisms", func(t *testing.T) {
+		truth := []byte{0, 0x76, 0, 1, E2E}
+		tmp, err := n.EncodeUserPlaneNodeStatus(SupportedDelayMechanisms)
+		if err != nil {
+			t.Errorf("Encode Supported Delay Mechanisms failed; got %v\n", err)
+		}
+		if !bytes.Equal(truth, tmp) {
+			t.Errorf("Encode Supported Delay Mechanisms, want %v but got %v", truth, tmp)
+		}
+	})
+
+	t.Run("Encode Port Status with PTP Grandmaster Capable", func(t *testing.T) {
+		truth := []byte{0, 0x77, 0, 1, TRUE}
+		tmp, err := n.EncodeUserPlaneNodeStatus(PTPGrandmasterCapable)
+		if err != nil {
+			t.Errorf("Encode Supported PTP Grandmaster Capable failed; got %v\n", err)
+		}
+		if !bytes.Equal(truth, tmp) {
+			t.Errorf("Encode Supported PTP Grandmaster Capable, want %v but got %v", truth, tmp)
+		}
+	})
+
+	t.Run("Encode Port Status with Supported PTP Profiles", func(t *testing.T) {
+		truth := []byte{0, 0x79, 0, 1, E2EDefault}
+		tmp, err := n.EncodeUserPlaneNodeStatus(SupportedPTPProfiles)
+		if err != nil {
+			t.Errorf("Encode Supported PTP Profiles failed; got %v\n", err)
+		}
+		if !bytes.Equal(truth, tmp) {
+			t.Errorf("Encode Supported PTP Profiles, want %v but got %v", truth, tmp)
+		}
+	})
+
+	// t.Run("Handle Manage Port Command GetCapabilities and ReadParameter ", func(t *testing.T) {
+	// 	truth := []byte{70, 0, 10, 0, 0xE2, 0, 0xE3, 0, 0xE4, 0, 0xE5, 0, 0xE7, 71, 0, 11, 2, 0, 227, 0, 1, 0, 0, 228, 0, 1, 1}
+	// 	tmp, err := n.HandleManagePortCommand([]byte{0, 0, 7, 1, 2, 0, 0xE3, 2, 0, 0xE4}, 2)
+	// 	if err != nil {
+	// 		t.Errorf("HandleManagePortCommand failed; got %v\n", err)
+	// 	}
+	// 	if !bytes.Equal(truth, tmp) {
+	// 		t.Errorf("Build PortStatus wrong, want %v but got %v", truth, tmp)
+	// 	}
+	// })
+}
